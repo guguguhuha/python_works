c049ff088616cf6928a71cf83ebedaca2904af2b
guguguhuha <guguguhuha@qq.com>
2021-03-28 17:02:54 +0800

    update

--- pc_test/装饰器之测试函数运行时间.py
+++ pc_test/装饰器之测试函数运行时间.py
@@ -7,18 +7,11 @@ def timing_func(f):
         return (stop - start)
     return wrapper
 
-<<<<<<< HEAD
 
 def fun1():
     print("lalala")
     time.sleep(1)
 fun1=timing_func(fun1)
-=======
-def fun1():
-    print("lalala")
-    time.sleep(1)
-fun1 = timong_func(fun1)
->>>>>>> 087b2ff9085292099cac11516e676a873e99562a
 
 @timing_func
 def fun2():
